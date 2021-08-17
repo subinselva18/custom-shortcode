721544b4f30d42350a3cc0620d33a2dd14a6f1f2
subinselva18 <84334388+subinselva18@users.noreply.github.com>
2021-08-17 14:43:32 +0530

    Update custom-shortcodes.php

--- custom-shortcodes.php
+++ custom-shortcodes.php
@@ -1,26 +1,10 @@
 <?php
-/*
-Plugin Name: Post Content Shortcodes
-Plugin URI: http://plugins.ten-321.com/post-content-shortcodes/
-Description: Adds shortcodes to show the content of another post or to show a list of posts
-Version: 1.0.1.2
-Author: cgrymala
-Author URI: http://ten-321.com/
-License: GPL2
-Text Domain: post-content-shortcodes
-Domain Path: /lang
-*/
-/**
- * Pull in the post_content_shortcodes class definition file
- */
-if( ! class_exists( '\Post_Content_Shortcodes' ) )
-	require_once( plugin_dir_path( __FILE__ ) . '/classes/class-post-content-shortcodes-admin.php' );
-if( ! class_exists( '\PCS_Widget' ) )
-	require_once( plugin_dir_path( __FILE__ ) . '/classes/class-post-content-widgets.php' );
-
-global $post_content_shortcodes_obj;
-if ( is_admin() ) {
-	$post_content_shortcodes_obj = \Post_Content_Shortcodes_Admin::instance();
-} else {
-	$post_content_shortcodes_obj = \Post_Content_Shortcodes::instance();
+add_shortcode('custom-shortcode','post-list');
+function post-list(){
+$args = array(
+'post_status' => 'publish',
+'orderby' => 'post_date',
+$old_post - new WP_Query( $args);
+print_r( $old_post->post);
+	
 }
