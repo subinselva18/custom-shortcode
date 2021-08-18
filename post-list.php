1497f2140d31c7917c07a0c4d09a425fa7253b1f
subinselva18 <84334388+subinselva18@users.noreply.github.com>
2021-08-18 10:04:23 +0530

    Update post-list.php

--- post-list.php
+++ post-list.php
@@ -1,16 +1,16 @@
 /**
  * Plugin Name:       custom-shortcode
- * Plugin URI:        https://example.com/plugins/the-basics/
- * Description:       Handle the basics with this plugin.
- * Version:           1.10.3
+ * Plugin URI:        https://sb.hyphenate.in//plugins/post-list/
+ * Description:       To list down last five post.
+ * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            John Smith
- * Author URI:        https://author.example.com/
+ * Author:            Subin Selva
+ * Author URI:        https://sb.hyphenate.in
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://example.com/my-plugin/
- * Text Domain:       my-basics-plugin
+ * Text Domain:       post-list
  * Domain Path:       /languages
  */
 
