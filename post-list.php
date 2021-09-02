b7391b39237beb8cfbbde02c965b7987f0809811
subinselva18 <84334388+subinselva18@users.noreply.github.com>
2021-09-02 15:51:56 +0530

    Update post-list.php

--- post-list.php
+++ post-list.php
@@ -14,6 +14,7 @@
  * Update URI:        https://example.com/my-plugin/
  * Text Domain:       post-list
  * Domain Path:       /languages
+ *
  * @since 0.0.0
  */
 
@@ -22,13 +23,13 @@ add_shortcode( 'custom_shortcode', 'post_list' );
 
 // function to test php file to return the list of 5 old post
 
-function post_list(){
-    $args = array(
-        'post_status'   => 'publish',
-        'post_per_page' => 5,
-        'orderby'       => 'post_date',
-        'order'         => 'DESC'
-    );
-    $old_post = new WP_Query( $args );
-    print_r( $old_post->post );
+function post_list() {
+	$args     = array(
+		'post_status'   => 'publish',
+		'post_per_page' => 5,
+		'orderby'       => 'post_date',
+		'order'         => 'DESC',
+	);
+	$old_post = new WP_Query( $args );
+	print_r( $old_post->post );
 }
