b68de2572fba7e6c3599847ef4316b5ed3522bb8
subinselva18 <84334388+subinselva18@users.noreply.github.com>
2021-09-15 15:22:38 +0530

    Update custom-shortcode.php

--- custom-shortcode.php
+++ custom-shortcode.php
@@ -22,29 +22,25 @@
  * Domain Path:       /languages
  **/
 
-// add custom shortcode.
-add_shortcode( 'custom_shortcode', 'custom_shortcode' );
+defined( 'ABSPATH' ) || exit;
 
-// Function To test php file to return the list of 5 old post.
+/**
+ * Plugin's file path
+ *
+ * @since 0.1.0
+ */
+define( 'CS_PATH', plugin_dir_path( __FILE__ ) );
 
-function custom_shortcode() {
-	// Declaring a $args variable and assigning the static values to the properties.
-	$args     = array(
-		'post_status'   => 'publish',
-		'post_per_page' => 5,
-		'orderby'       => 'post_date',
-		'order'         => 'DESC',
-	);
-	$old_post = new WP_Query( $args );
-	// Printing 5 oldest post.
-	print_r( $old_post->post );
-	// Loops to get post from $old_post.
-	foreach ( $old_post->posts as $old_post ) {
-		/**
-		 * Includes the template file.
-		 *
-		 * @since 1.0.0
-		 */
-		include 'templates/custom-shortcode.php';
-	}
-}
+/**
+ * Plugin's url path
+ *
+ * @since 0.1.0
+ */
+define( 'CS_URL', plugin_dir_url( __FILE__ ) );
+
+/**
+ * Includes the main functionality file.
+ *
+ * @since 1.0.0
+ */
+require_once 'includes/shortcode.php';
