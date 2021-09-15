71872f879567d7cfb34402cd2bd0317d12863947
subinselva18 <84334388+subinselva18@users.noreply.github.com>
2021-09-15 15:05:39 +0530

    Create custom-shortcode.php

---
+++ templates/custom-shortcode.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Custom Shortcode Template.
+ *
+ * @package custom-shortcode/templates
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
+?>
+<article>
+	<h2><?php echo esc_html( $old_post->post_title ); ?></h2>
+	<p><?php echo esc_html( $old_post->post_excerpt ); ?></p>
+	<img src="<?php echo esc_html( get_the_post_thumbnail_url( $old_post, 'medium' ) ); ?>"/>
+</article>
